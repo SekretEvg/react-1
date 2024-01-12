71025e1270ecce6f80c690c848267fefb871f088
SekretEvg <sekret_pk@mail.ru>
2024-01-12 11:43:33 +0800

    32 add addPost

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import state from './redux/state';
-
+import {addPost} from './redux/state';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App state={state}/>
+    <App state={state} addPost={addPost}/>
 );
 
 // If you want to start measuring performance in your app, pass a function
