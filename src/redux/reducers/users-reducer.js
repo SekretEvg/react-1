3d44816adcb62a72cfd71df16dc91b2a7119ba42
SekretEvg <sekret_pk@mail.ru>
2024-02-18 19:21:37 +0800

    add pagination users

--- src/redux/reducers/users-reducer.js
+++ src/redux/reducers/users-reducer.js
@@ -1,34 +1,14 @@
 const SET_USERS = 'SET_USERS';
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
 
 const initialState = {
-    users: [
-        // {
-        //     id: 1,
-        //     photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/%D0%9D%D0%B0%D0%B3%D0%B8%D0%B5%D0%B2_%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9.jpg/220px-%D0%9D%D0%B0%D0%B3%D0%B8%D0%B5%D0%B2_%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9.jpg',
-        //     isFollowed: false,
-        //     fullName: 'Evgeny',
-        //     status: 'I am a boss',
-        //     location: {city: 'Moscow', country: 'Russia'}
-        // },
-        // {
-        //     id: 2,
-        //     photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/%D0%9D%D0%B0%D0%B3%D0%B8%D0%B5%D0%B2_%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9.jpg/220px-%D0%9D%D0%B0%D0%B3%D0%B8%D0%B5%D0%B2_%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9.jpg',
-        //     isFollowed: true,
-        //     fullName: 'Sasha',
-        //     status: 'I am a boss',
-        //     location: {city: 'New-York', country: 'USA'}
-        // },
-        // {
-        //     id: 3,
-        //     photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/%D0%9D%D0%B0%D0%B3%D0%B8%D0%B5%D0%B2_%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9.jpg/220px-%D0%9D%D0%B0%D0%B3%D0%B8%D0%B5%D0%B2_%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%B8%D0%B9.jpg',
-        //     isFollowed: false,
-        //     fullName: 'Dimasik',
-        //     status: 'I am a boss',
-        //     location: {city: 'Kiev', country: 'Ukraine'}
-        // },
-    ],
+    users: [],
+    pageSize: 100,
+    totalUsersCount: 0,
+    currentPage: 1,
 };
 
 const usersReducer = (state = initialState, action) => {
@@ -54,7 +34,11 @@ const usersReducer = (state = initialState, action) => {
                 })
             };
         case SET_USERS:
-            return {...state, users: [...state.users, ...action.users]};
+            return {...state, users: action.users};
+        case SET_CURRENT_PAGE:
+            return {...state, currentPage: action.currentPage};
+        case SET_TOTAL_USERS_COUNT:
+            return {...state, totalUsersCount: action.totalUsersCount}
 
         default:
             return state;
@@ -64,5 +48,8 @@ export const followAC = (userId) => ({type: FOLLOW, userId});
 export const unfollowAC = (userId) => ({type: UNFOLLOW, userId});
 
 export const setUsersAC = (users) => ({type: SET_USERS, users});
+
+export const setCurrentPageAC = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage});
+export const setTotalUsersCountAC = (totalUsersCount) => ({type: SET_TOTAL_USERS_COUNT, totalUsersCount});
 
 export default usersReducer;
