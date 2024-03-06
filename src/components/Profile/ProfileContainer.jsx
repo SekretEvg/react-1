beb405d75ad067f8b77fab61d67da0bc492a374a
SekretEvg <sekret_pk@mail.ru>
2024-03-06 13:30:03 +0800

    add editMode local state

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -40,5 +40,5 @@ function withRouter(Component) {
 export default compose(
     connect(mapStateToProps, {getUserProfile}),
     withRouter,
-    withAuthRedirect
+    // withAuthRedirect
 )(ProfileContainer);
